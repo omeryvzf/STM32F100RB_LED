076ae7b1e1a0f6fa46b608d4ea4a7b509eab8604
Ömer Faruk Yavuz <farukfaruk1292@gmail.com>
2023-07-23 22:02:04 +0300

    STM32F100RB_LED
    LED blinking application.I used keil and stdperiph library.

---
+++ main.c
@@ -0,0 +1,37 @@
+#include "stm32f10x.h"                  // Device header
+
+void gpioConfig(){
+	GPIO_InitTypeDef	GPIOInitStructure; 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE); // portu aktif ettik clocklari tetikledik
+	
+	GPIOInitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIOInitStructure.GPIO_Pin=GPIO_Pin_0;
+	GPIOInitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	
+	GPIO_Init(GPIOB,&GPIOInitStructure);
+	
+	
+}
+void delay(uint32_t time){
+	while(time--){
+	}
+}
+
+int main(){
+	gpioConfig();
+	while(1){
+		
+//		GPIO_SetBits(GPIOB,GPIO_Pin_0);
+//		delay(360000);
+//		GPIO_ResetBits(GPIOB,GPIO_Pin_0);
+//		delay(360000);
+		
+		GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_SET);
+		delay(3600000);
+		GPIO_WriteBit(GPIOB,GPIO_Pin_0,Bit_RESET);
+		delay(3600000);
+	
+	
+	}
+
+}
